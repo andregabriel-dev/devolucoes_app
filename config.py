b7699be65684d7096836a531659f460fe4eb6b09
André Oliveira <andre.oliveira@mic.ind.br>
2026-03-02 16:31:22 -0300

    Atualização de db

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = 'sua_chave_super_secreta_aqui'
+    # Ele tenta pegar a chave do Render, se não tiver, usa 'chave-muito-secreta'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'chave-muito-secreta-de-teste'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
